4e1597129d745ebf8c466da36dc892e97005134b
Ibra <ibra@MacBook-Pro-6.local>
2018-10-07 21:20:17 +0100

    board can add turns.

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -23,4 +23,23 @@ describe('Board', () => {
     it('should have an accessible gameBoard property', () => {
         expect(board.gameBoard).toEqual(["1","2","3","4","5","6","7","8","9"])
     });
+
+    describe('getAvailableSpaces', () => {
+        it('should return available spaces on the gameboard', () => {
+            expect(board.getAvailableSpaces()).toEqual([0,1,2,3,4,5,6,7,8])
+        });
+    });
+
+    describe('addTurn', () => {
+        it('should add a players weapon type to the gameboard array', () => {
+            board.addTurn(0, "X");
+            expect(board.gameBoard).toEqual(["X","2","3","4","5","6","7","8","9"])
+        });
+        it('should not add a player weapon type if the position is taken', () => {
+            board.gameBoard[0] = "O";
+            board.addTurn(0, "X");
+            expect(board.gameBoard).toEqual(["O","2","3","4","5","6","7","8","9"])
+        });
+    });
+
 });
--- src/player.js
+++ src/player.js
@@ -4,5 +4,35 @@ const Player = (name, weapon) => {
 
 const Board = () => {
     let gameBoard = ["1","2","3","4","5","6","7","8","9"]
-    return ({gameBoard})
+    const getAvailableSpaces = () => {
+        spaces = [];
+        gameBoard.forEach(function(element, index){
+            if (isAvailablePosition(index)) {
+                spaces.push(index);
+            };
+        });
+        return spaces;
+    };
+    const addTurn = (position, playerWeapon) => {
+        if (isAvailablePosition(position)) {
+            gameBoard[position] = playerWeapon;
+        }
+    }
+    const WIN_CONDITIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    const isAvailablePosition = (position) => {
+        return gameBoard[position] !== "X" && gameBoard[position] !== "O";
+    }
+
+
+    return ({gameBoard, getAvailableSpaces, addTurn})
 }
